5de00f40a082883f6bad054cd696984902cafccf
Victor Atavila <victoratavila@hotmail.com>
2020-10-20 22:23:20 -0300

    Second heroku commit

--- converter/src/app.js
+++ converter/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const router = require('./routes/router');
 const path = require('path');
-const port = 3000;
+const port = process.env.PORT || 5000;
 
 // Setting routes
 app.use('/', router);
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 // Server listener
-app.listen(process.env.port || port, () => {
+app.listen(port, () => {
     console.log('The server is running');
 })
 
